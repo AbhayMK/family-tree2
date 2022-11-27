03803a7ac280e607bcd25a35883375ce337d4a2d
AbhayMK <abhay.physics@gmail.com>
2022-11-27 17:27:34 +0530

    firstr changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
-import addMember from './addMember.js';
-import './App.css';
+import addMember from "./addMember.js";
+import "./App.css";
 
 const memberList = document.getElementById("memberList");
-let memberList2 =[];
+let memberList2 = [];
 
-function onInputChange(event){
-  memberList.textContent=event.target.value;
+function onInputChange(event) {
+  memberList.textContent = event.target.value;
   console.log(event.target.value);
   // memberList2.forEach(nam){(nam)=>{console.log(nam)}};
 }
 
-function addMember2(nam){
+function addMember2(nam) {
   memberList2.push(nam);
   return nam;
 }
@@ -22,12 +21,17 @@ function App() {
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <h1>Family Tree</h1>
-        <input className='pa3 ba b--green bg-lightest-blue'type='search' placeholder='add family member' onChange={onInputChange}  />
+        <input
+          className="pa3 ba b--green bg-lightest-blue"
+          type="search"
+          placeholder="add family member"
+          onChange={onInputChange}
+        />
         <button onClick={addMember2}>Add a Member</button>
-        <p id='memberList'> Text </p>
-        <div id='memberList2'>
-        <p>Test</p>
-         {/*{{ memberList2.forEach((nam)=>
+        <p id="memberList"> Text </p>
+        <div id="memberList2">
+          <p>Test</p>
+          {/*{{ memberList2.forEach((nam)=>
             {console.log(nam)})
        }}*/}
         </div>
